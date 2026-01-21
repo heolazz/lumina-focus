08c1066d59266fd9c3d46220e06461b74b917c99
heolazz <heolazz.gmail.com>
2026-01-21 09:49:10 +0700

    chore: secure api keys and prep for deployment

--- supabase.ts
+++ supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-// GANTI INI DENGAN DATA DARI SUPABASE DASHBOARD ANDA
-const supabaseUrl = 'https://yqzinndqwvhmhokxnqyz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxemlubmRxd3ZobWhva3hucXl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5MjE5NDcsImV4cCI6MjA4NDQ5Nzk0N30.3cyRl470d_YyPgyV_OrLFOHeCIi-GtruOhCb07oqUCw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase Environment Variables');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
